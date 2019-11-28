62e5be537707dee8071a375906a17fcf20d6522a
Juan Otalora <juancarlos.otalora@wnco.com>
2019-11-28 18:44:02 -0500

    Lyfe-cycle methods with annotations

---
+++ springcore/src/main/java/com/bharath/spring/springcore/lc/annotations/Test.java
@@ -0,0 +1,17 @@
+package com.bharath.spring.springcore.lc.annotations;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/bharath/spring/springcore/lc/annotations/lcannotationsconfig.xml");
+		Patient patient = (Patient) context.getBean("patient");
+		System.out.println(patient);
+		context.registerShutdownHook();
+
+	}
+
+}
