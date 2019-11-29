65ff4bd7cb81c4d62658fc7c51adb6a2e498e12c
Juan Otalora <juancarlos.otalora@wnco.com>
2019-11-29 10:57:00 -0500

    Inner beans

---
+++ springcore/src/main/java/com/bharath/spring/springcore/innerbeans/Test.java
@@ -0,0 +1,16 @@
+package com.bharath.spring.springcore.innerbeans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/bharath/spring/springcore/innerbeans/innerbeansconfig.xml");
+		Employee employee = (Employee) context.getBean("employee");
+		System.out.println(employee);
+
+	}
+
+}
