d3d62de9b5bbfd449838a55e8800a51d4042202f
jcotalorac <jcotalorac@localhost>
2019-11-30 21:54:29 -0500

    Resolution of constructor args by type

--- springcore/src/main/java/com/bharath/spring/springcore/constructorinjection/ambiguity/Addition.java
+++ springcore/src/main/java/com/bharath/spring/springcore/constructorinjection/ambiguity/Addition.java
@@ -3,15 +3,9 @@ package com.bharath.spring.springcore.constructorinjection.ambiguity;
 public class Addition {
 	
 
-	public Addition(double a, double b) {
-		System.out.println("Inside constructor DOUBLE");
-	}
-
-	public Addition(int a, int b) {
-		System.out.println("Inside constructor INT");
-	}
-	
-	public Addition(String a, String b) {
-		System.out.println("Inside constructor STRING");
+	public Addition(int a, double b) {
+		System.out.println("Inside constructor");
+		System.out.println(a);
+		System.out.println(b);
 	}
 }
