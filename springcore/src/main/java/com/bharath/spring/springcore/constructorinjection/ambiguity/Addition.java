8ad2dbce38c546fb455465c117ad5d3e8df9cc6d
jcotalorac <jcotalorac@localhost>
2019-11-30 21:32:44 -0500

    First constructor in order is invoked

--- springcore/src/main/java/com/bharath/spring/springcore/constructorinjection/ambiguity/Addition.java
+++ springcore/src/main/java/com/bharath/spring/springcore/constructorinjection/ambiguity/Addition.java
@@ -2,11 +2,12 @@ package com.bharath.spring.springcore.constructorinjection.ambiguity;
 
 public class Addition {
 	
-	public Addition(int a, int b) {
-		System.out.println("Inside constructor INT");
-	}
 
 	public Addition(double a, double b) {
 		System.out.println("Inside constructor DOUBLE");
+	}
+
+	public Addition(int a, int b) {
+		System.out.println("Inside constructor INT");
 	}
 }
