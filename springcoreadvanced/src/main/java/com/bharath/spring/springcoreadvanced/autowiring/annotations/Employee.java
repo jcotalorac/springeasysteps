2eb529c900bc191f33c5c69dde1770a83021b7fa
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-02 15:20:31 -0500

    Autowired with qualifier existent bean

--- springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/autowiring/annotations/Employee.java
+++ springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/autowiring/annotations/Employee.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 public class Employee {
 
 	@Autowired
-	@Qualifier("address789")
+	@Qualifier("address123")
 	private Address address;
 
 	public Address getAddress() {
