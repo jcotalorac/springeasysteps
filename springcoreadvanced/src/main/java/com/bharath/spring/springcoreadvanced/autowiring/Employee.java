3aa4426f6ebc547b67cea3a3e8cc5498545a57c3
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-02 14:31:15 -0500

    Autowiring by constructor

--- springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/autowiring/Employee.java
+++ springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/autowiring/Employee.java
@@ -4,6 +4,11 @@ public class Employee {
 
 	private Address address;
 
+	public Employee(Address address) {
+		super();
+		this.address = address;
+	}
+
 	public Address getAddress() {
 		return address;
 	}
