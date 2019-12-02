8e5ccd139bcb637c51833c6b5541a8767aea956d
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-02 14:50:27 -0500

    Autowire by field level

--- springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/autowiring/annotations/Employee.java
+++ springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/autowiring/annotations/Employee.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class Employee {
 
+	@Autowired
 	private Address address;
 
 	public Address getAddress() {
 		return address;
 	}
 
-	@Autowired
 	public void setAddress(Address address) {
 		this.address = address;
 	}
