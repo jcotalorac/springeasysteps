fe5e4285c0c9d6dd672417130a6f692882907623
jcotalorac <jcotalorac@localhost>
2019-12-02 22:27:44 -0500

    Dependency POJO

---
+++ springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/stereotype/annotations/Profile.java
@@ -0,0 +1,28 @@
+package com.bharath.spring.springcoreadvanced.stereotype.annotations;
+
+public class Profile {
+
+	private String title;
+	private String company;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	@Override
+	public String toString() {
+		return "Profile [title=" + title + ", company=" + company + "]";
+	}
+}
