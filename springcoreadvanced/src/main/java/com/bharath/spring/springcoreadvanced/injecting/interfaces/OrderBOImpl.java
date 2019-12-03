59737e9c5a994b0989f4d088fdded96a5a31589e
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-03 11:29:48 -0500

    Linkage between classes

--- springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/injecting/interfaces/OrderBOImpl.java
+++ springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/injecting/interfaces/OrderBOImpl.java
@@ -2,9 +2,20 @@ package com.bharath.spring.springcoreadvanced.injecting.interfaces;
 
 public class OrderBOImpl implements OrderBO {
 
+	private OrderDAO dao;
+
+	public OrderDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(OrderDAO dao) {
+		this.dao = dao;
+	}
+
 	@Override
 	public void placeOrder() {
 		System.out.println("Inside Order BO placeOrder()");
+		dao.createOrder();
 	}
 
 }
