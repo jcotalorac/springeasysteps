d959363a5ea8a26168ebdcaf513cf0a0a4f6029d
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-03 12:15:10 -0500

    A way for resolution is the use of qualifier, choosing the proper implementation by name default

--- springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/injecting/interfaces/OrderBOImpl.java
+++ springcoreadvanced/src/main/java/com/bharath/spring/springcoreadvanced/injecting/interfaces/OrderBOImpl.java
@@ -1,12 +1,14 @@
 package com.bharath.spring.springcoreadvanced.injecting.interfaces;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("bo")
 public class OrderBOImpl implements OrderBO {
 
 	@Autowired
+	@Qualifier("orderDAOImpl2")
 	private OrderDAO dao;
 
 	public OrderDAO getDao() {
