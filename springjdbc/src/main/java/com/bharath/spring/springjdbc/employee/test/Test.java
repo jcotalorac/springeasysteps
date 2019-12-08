5b1bd8a1769a5323e4076726e7af2367ced106cb
jcotalorac <jcotalorac@localhost>
2019-12-07 21:29:08 -0500

    Bad name of attribute jdbcTemplate in dao implementation class

--- springjdbc/src/main/java/com/bharath/spring/springjdbc/employee/test/Test.java
+++ springjdbc/src/main/java/com/bharath/spring/springjdbc/employee/test/Test.java
@@ -4,13 +4,23 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.bharath.spring.springjdbc.employee.dao.EmployeeDao;
+import com.bharath.spring.springjdbc.employee.dto.Employee;
+
 public class Test {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/bharath/spring/springjdbc/jdbcconfig.xml");
-		JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
-		
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/bharath/spring/springjdbc/employee/test/jdbcconfig.xml");
+		EmployeeDao employeeDao = (EmployeeDao) context.getBean("employeeDao");
+		Employee employee = new Employee();
+		employee.setId(2);
+		employee.setFirstName("John");
+		employee.setLastName("Ferguson");
+		int result = employeeDao.create(employee);
+		System.out.println("Number of records inserted are: " + result);
+
 	}
 
 }
