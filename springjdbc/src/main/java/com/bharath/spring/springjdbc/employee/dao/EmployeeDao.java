30e6b05c79f9011c7e64d9c6868501f9a0442835
jcotalorac <jcotalorac@localhost>
2019-12-07 21:00:18 -0500

    DTO and DAO classes

---
+++ springjdbc/src/main/java/com/bharath/spring/springjdbc/employee/dao/EmployeeDao.java
@@ -0,0 +1,8 @@
+package com.bharath.spring.springjdbc.employee.dao;
+
+import com.bharath.spring.springjdbc.employee.dto.Employee;
+
+public interface EmployeeDao {
+
+	int create(Employee employee);
+}
---
+++ springjdbc/src/main/java/com/bharath/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
@@ -0,0 +1,13 @@
+package com.bharath.spring.springjdbc.employee.dao.impl;
+
+import com.bharath.spring.springjdbc.employee.dao.EmployeeDao;
+import com.bharath.spring.springjdbc.employee.dto.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+
+	@Override
+	public int create(Employee employee) {
+		return 0;
+	}
+
+}
