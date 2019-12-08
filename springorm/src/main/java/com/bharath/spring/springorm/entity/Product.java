8ebb13e4980aa9c4b625ec6f84df5c3d6c0d8871
jcotalorac <jcotalorac@localhost>
2019-12-08 18:45:43 -0500

    Table annotation used optionally if table name is different from class name

--- springorm/src/main/java/com/bharath/spring/springorm/entity/Product.java
+++ springorm/src/main/java/com/bharath/spring/springorm/entity/Product.java
@@ -2,8 +2,10 @@ package com.bharath.spring.springorm.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="product")
 public class Product {
 
 	@Id
