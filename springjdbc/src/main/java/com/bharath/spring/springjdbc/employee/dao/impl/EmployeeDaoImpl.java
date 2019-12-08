dd545fbef9540344a640385fddabea8c75229678
jcotalorac <jcotalorac@localhost>
2019-12-07 21:08:37 -0500

    Use of jdbcTemplate in dao implementation

--- springjdbc/src/main/java/com/bharath/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
+++ springjdbc/src/main/java/com/bharath/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
@@ -1,13 +1,27 @@
 package com.bharath.spring.springjdbc.employee.dao.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.bharath.spring.springjdbc.employee.dao.EmployeeDao;
 import com.bharath.spring.springjdbc.employee.dto.Employee;
 
 public class EmployeeDaoImpl implements EmployeeDao {
 
+	private JdbcTemplate jcbcTemplate;
+
+	public JdbcTemplate getJcbcTemplate() {
+		return jcbcTemplate;
+	}
+
+	public void setJcbcTemplate(JdbcTemplate jcbcTemplate) {
+		this.jcbcTemplate = jcbcTemplate;
+	}
+
 	@Override
 	public int create(Employee employee) {
-		return 0;
+		String sql = "insert into employee values(?, ?, ?)";
+		int result = jcbcTemplate.update(sql, employee.getId(), employee.getFirstName(), employee.getLastName());
+		return result;
 	}
 
 }
