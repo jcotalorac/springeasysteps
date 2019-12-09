c41dafb9620b07b2e4b38a667eadf516ddd0d29a
jcotalorac <jcotalorac@localhost>
2019-12-08 19:17:16 -0500

    Hibernate template in dao implementation

--- springorm/src/main/java/com/bharath/spring/springorm/product/dao/impl/ProductDaoImpl.java
+++ springorm/src/main/java/com/bharath/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -1,13 +1,16 @@
 package com.bharath.spring.springorm.product.dao.impl;
 
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
 import com.bharath.spring.springorm.product.dao.ProductDao;
 import com.bharath.spring.springorm.product.entity.Product;
 
 public class ProductDaoImpl implements ProductDao {
 
+	private HibernateTemplate hibernateTemplate;
+	
 	@Override
 	public int create(Product product) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
