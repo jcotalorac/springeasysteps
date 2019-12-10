badc468a6e4ccdd9319db235b78e4dfb63b846ee
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-10 16:16:55 -0500

    Implementation of create method inside transaction and using hibernate template

--- springorm/src/main/java/com/bharath/spring/springorm/product/dao/impl/ProductDaoImpl.java
+++ springorm/src/main/java/com/bharath/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -3,6 +3,7 @@ package com.bharath.spring.springorm.product.dao.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bharath.spring.springorm.product.dao.ProductDao;
 import com.bharath.spring.springorm.product.entity.Product;
@@ -14,8 +15,10 @@ public class ProductDaoImpl implements ProductDao {
 	private HibernateTemplate hibernateTemplate;
 	
 	@Override
+	@Transactional
 	public int create(Product product) {
-		return 0;
+		Integer result = (Integer) hibernateTemplate.save(product);
+		return result;
 	}
 
 }
