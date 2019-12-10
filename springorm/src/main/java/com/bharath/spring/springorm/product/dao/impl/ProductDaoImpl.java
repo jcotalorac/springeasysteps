c923e66e1c790b1b0bc01d1e259775600e19736a
jcotalorac <jcotalorac@localhost>
2019-12-09 22:40:07 -0500

    Annotations for implementation bean and autowiring of hibernate template

--- springorm/src/main/java/com/bharath/spring/springorm/product/dao/impl/ProductDaoImpl.java
+++ springorm/src/main/java/com/bharath/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -1,12 +1,16 @@
 package com.bharath.spring.springorm.product.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
 import com.bharath.spring.springorm.product.dao.ProductDao;
 import com.bharath.spring.springorm.product.entity.Product;
 
+@Component
 public class ProductDaoImpl implements ProductDao {
 
+	@Autowired
 	private HibernateTemplate hibernateTemplate;
 	
 	@Override
