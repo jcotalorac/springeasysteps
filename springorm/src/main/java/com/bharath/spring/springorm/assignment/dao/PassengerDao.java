43ddfb8b24c5a5135324eef2574c9930ca556085
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-11 11:37:58 -0500

    Dao declaration and implementation of operations with hibernate template

---
+++ springorm/src/main/java/com/bharath/spring/springorm/assignment/dao/PassengerDao.java
@@ -0,0 +1,18 @@
+package com.bharath.spring.springorm.assignment.dao;
+
+import java.util.List;
+
+import com.bharath.spring.springorm.assignment.entity.Passenger;
+
+public interface PassengerDao {
+
+	int create(Passenger passenger);
+	
+	int update(Passenger passenger);
+	
+	int delete(int id);
+	
+	Passenger find(int id);
+	
+	List<Passenger> findAll();
+}
---
+++ springorm/src/main/java/com/bharath/spring/springorm/assignment/dao/impl/PassengerDaoImpl.java
@@ -0,0 +1,53 @@
+package com.bharath.spring.springorm.assignment.dao.impl;
+
+import java.util.List;
+
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+import com.bharath.spring.springorm.assignment.dao.PassengerDao;
+import com.bharath.spring.springorm.assignment.entity.Passenger;
+
+@Component("passengerDao")
+public class PassengerDaoImpl implements PassengerDao {
+
+	private HibernateTemplate hibernateTemplate;
+	
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+
+	@Override
+	public int create(Passenger passenger) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Passenger passenger) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Passenger find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Passenger> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
