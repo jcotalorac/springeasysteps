51037dc5af701c595763b3ea10a8e53506c05440
jcotalorac <jcotalorac@localhost>
2019-12-14 17:14:56 -0500

    Setting viewname to modelAndView controller method

--- springmvc/src/main/java/com/bharath/spring/springmvc/HelloController.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/HelloController.java
@@ -1,8 +1,14 @@
 package com.bharath.spring.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
+	public ModelAndView hello() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("hello");
+		return modelAndView;		
+	}
 }
