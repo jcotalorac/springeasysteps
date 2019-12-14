28a36897de7974e693fad69602750e8cef9e6af7
jcotalorac <jcotalorac@localhost>
2019-12-14 17:15:52 -0500

    Url pattern to respond and execute the controller method

--- springmvc/src/main/java/com/bharath/spring/springmvc/HelloController.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/HelloController.java
@@ -1,11 +1,13 @@
 package com.bharath.spring.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
+	@RequestMapping("/hello")
 	public ModelAndView hello() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("hello");
