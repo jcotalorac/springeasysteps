76dd757b5d2a95a15db200346beb9beee35e6e7b
jcotalorac <jcotalorac@localhost>
2019-12-14 22:48:32 -0500

    Use of model map and string view

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/UserController.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.bharath.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,12 +18,9 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
-	public ModelAndView registerUser(@ModelAttribute("user") User user) {
+	public String registerUser(@ModelAttribute("user") User user, ModelMap model) {
 		System.out.println(user);
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("user", user);
-		
-		modelAndView.setViewName("regResult");
-		return modelAndView;
+		model.addAttribute("user", user);
+		return "regResult";
 	}
 }
