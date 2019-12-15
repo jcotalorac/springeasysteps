5b71f797cc9d20cc098a25ab421a893b1dce041d
jcotalorac <jcotalorac@localhost>
2019-12-14 21:00:51 -0500

    Setting object to the model and view

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/ObjectController.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/ObjectController.java
@@ -3,12 +3,20 @@ package com.bharath.spring.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bharath.spring.springmvc.dto.Employee;
+
 @Controller
 public class ObjectController {
 
 	public ModelAndView sendObject() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("displayObject");
+
+		Employee employee = new Employee();
+		employee.setId(123);
+		employee.setName("John");
+		employee.setSalary(8000);
+		modelAndView.addObject("employee", employee);
 		return modelAndView;
 	}
 }
