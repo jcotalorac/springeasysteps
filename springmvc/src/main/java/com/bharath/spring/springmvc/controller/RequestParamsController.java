faae141305751b21d60b4871610031bb63fbb8a3
jcotalorac <jcotalorac@localhost>
2019-12-14 22:25:34 -0500

    Controller to manage request params from query params

---
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/RequestParamsController.java
@@ -0,0 +1,18 @@
+package com.bharath.spring.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RequestParamsController {
+
+	@RequestMapping("showData")
+	public ModelAndView showData(@RequestParam("id") int id, @RequestParam 
+			("name") String name, @RequestParam("salary") double salary) {
+		ModelAndView modelAndView = new ModelAndView();
+		return modelAndView;
+	}
+	
+}
