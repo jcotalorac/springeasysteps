e4d935de94db93ce92dcc536f1ecc6ba61eafa55
jcotalorac <jcotalorac@localhost>
2019-12-14 21:02:11 -0500

    Request mapping tag to process method

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/ObjectController.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/ObjectController.java
@@ -1,6 +1,7 @@
 package com.bharath.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bharath.spring.springmvc.dto.Employee;
@@ -8,6 +9,7 @@ import com.bharath.spring.springmvc.dto.Employee;
 @Controller
 public class ObjectController {
 
+	@RequestMapping("/readObject")
 	public ModelAndView sendObject() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("displayObject");
