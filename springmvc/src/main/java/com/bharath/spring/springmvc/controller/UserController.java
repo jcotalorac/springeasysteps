a8f4d66d90c9b4260b9e8ce7e66de82ee338cb6b
jcotalorac <jcotalorac@localhost>
2019-12-14 21:54:51 -0500

    Mapping url

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/UserController.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.bharath.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
+	@RequestMapping("registrationPage")
 	public ModelAndView showRegistrationPage() {
 		ModelAndView modelAndView = new ModelAndView();
 		
