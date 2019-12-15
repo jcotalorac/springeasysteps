18ab66651810692e537fc97c82df72a24be79708
jcotalorac <jcotalorac@localhost>
2019-12-14 20:59:12 -0500

    Controller method to send object. View setting

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/ObjectController.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/ObjectController.java
@@ -1,8 +1,14 @@
 package com.bharath.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ObjectController {
 
+	public ModelAndView sendObject() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("displayObject");
+		return modelAndView;
+	}
 }
