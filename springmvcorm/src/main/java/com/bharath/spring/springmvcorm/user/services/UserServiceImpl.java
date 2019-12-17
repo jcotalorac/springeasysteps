1a5d4095b994930a2526f8f8b13c0244358faee9
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 14:59:47 -0500

    Dao dependency in user service

--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserServiceImpl.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserServiceImpl.java
@@ -2,7 +2,18 @@ package com.bharath.spring.springmvcorm.user.services;
 
 import org.springframework.stereotype.Service;
 
+import com.bharath.spring.springmvcorm.user.dao.UserDao;
+
 @Service
 public class UserServiceImpl implements UserService {
 
+	private UserDao dao;
+
+	public UserDao getDao() {
+		return dao;
+	}
+
+	public void setDao(UserDao dao) {
+		this.dao = dao;
+	}
 }
