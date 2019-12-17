d1da3b8929481618d6ae74943a0b0bf36ffaf16e
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 14:49:06 -0500

    Dao implementation

---
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.bharath.spring.springmvcorm.user.dao;
+
+public interface UserDao {
+
+}
---
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/dao/impl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package com.bharath.spring.springmvcorm.user.dao.impl;
+
+import com.bharath.spring.springmvcorm.user.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+}
