0403f069651ac166741d53c0a4de570b3ad14a1d
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 14:38:51 -0500

    Entity and id annotation. Column annotations optional if the table fields names are not the same as class fields

--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/entity/User.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/entity/User.java
@@ -1,9 +1,20 @@
 package com.bharath.spring.springmvcorm.user.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
 
+	@Id
+	@Column(name = "id")
 	private int id;
+	@Column(name = "name")
 	private String name;
+	@Column(name = "email")
 	private String email;
 
 	public int getId() {
