b67bb8f2961722b2c4d1cdab53ee531e3cbc06a3
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 15:05:29 -0500

    User service dependency in user controller

--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/controller/UserController.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/controller/UserController.java
@@ -2,7 +2,18 @@ package com.bharath.spring.springmvcorm.user.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.bharath.spring.springmvcorm.user.services.UserService;
+
 @Controller
 public class UserController {
 
+	private UserService service;
+
+	public UserService getService() {
+		return service;
+	}
+
+	public void setService(UserService service) {
+		this.service = service;
+	}
 }
