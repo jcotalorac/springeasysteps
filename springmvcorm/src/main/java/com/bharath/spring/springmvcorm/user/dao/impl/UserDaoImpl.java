33b689703706bcd1c5493efdfc6f469aed9f1bf4
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 15:00:48 -0500

    Hibernate template autowiring

--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/dao/impl/UserDaoImpl.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/dao/impl/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package com.bharath.spring.springmvcorm.user.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import com.bharath.spring.springmvcorm.user.dao.UserDao;
 @Repository
 public class UserDaoImpl implements UserDao {
 
+	@Autowired
 	private HibernateTemplate hibernateTemplate;
 
 	public HibernateTemplate getHibernateTemplate() {
