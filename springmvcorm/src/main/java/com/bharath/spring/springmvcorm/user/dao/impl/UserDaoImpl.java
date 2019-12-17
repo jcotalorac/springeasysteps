a4bfdbac0b70a71fc1e3bf55c511cc84d46179c5
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 14:50:04 -0500

    Repository annotation marking implementation class

--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/dao/impl/UserDaoImpl.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/dao/impl/UserDaoImpl.java
@@ -1,7 +1,10 @@
 package com.bharath.spring.springmvcorm.user.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.bharath.spring.springmvcorm.user.dao.UserDao;
 
+@Repository
 public class UserDaoImpl implements UserDao {
 
 }
