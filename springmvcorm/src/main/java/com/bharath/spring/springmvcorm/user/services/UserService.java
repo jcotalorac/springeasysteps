6bdf5a55da0598ee17c1809454dbb8b6c5a73a47
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 14:56:47 -0500

    User services POJOs

---
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserService.java
@@ -0,0 +1,5 @@
+package com.bharath.spring.springmvcorm.user.services;
+
+public interface UserService {
+
+}
---
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package com.bharath.spring.springmvcorm.user.services;
+
+public class UserServiceImpl implements UserService {
+
+}
