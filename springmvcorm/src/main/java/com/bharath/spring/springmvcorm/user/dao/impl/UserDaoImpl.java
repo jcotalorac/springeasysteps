c6db13c726a8cfb9908da1d7698dbff0d6e5579a
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 14:51:24 -0500

    Hibernate template field in dao implementation

--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/dao/impl/UserDaoImpl.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/dao/impl/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package com.bharath.spring.springmvcorm.user.dao.impl;
 
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.bharath.spring.springmvcorm.user.dao.UserDao;
@@ -7,4 +8,13 @@ import com.bharath.spring.springmvcorm.user.dao.UserDao;
 @Repository
 public class UserDaoImpl implements UserDao {
 
+	private HibernateTemplate hibernateTemplate;
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
 }
