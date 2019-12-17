0e20f1b8bcab13185c3a7e391731d7b2c0bcb4b4
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-17 15:00:10 -0500

    Autowiring of user dao in user service

--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserServiceImpl.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bharath.spring.springmvcorm.user.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bharath.spring.springmvcorm.user.dao.UserDao;
@@ -7,6 +8,7 @@ import com.bharath.spring.springmvcorm.user.dao.UserDao;
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
 	private UserDao dao;
 
 	public UserDao getDao() {
