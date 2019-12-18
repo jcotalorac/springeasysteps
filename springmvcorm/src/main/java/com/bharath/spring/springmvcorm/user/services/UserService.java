ef1d20cbdaac616f179dae722efa7af76197e7db
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-18 09:38:02 -0500

    Save user service implementation

--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserService.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserService.java
@@ -1,5 +1,8 @@
 package com.bharath.spring.springmvcorm.user.services;
 
+import com.bharath.spring.springmvcorm.user.entity.User;
+
 public interface UserService {
 
+	int save(User user);
 }
--- springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserServiceImpl.java
+++ springmvcorm/src/main/java/com/bharath/spring/springmvcorm/user/services/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bharath.spring.springmvcorm.user.dao.UserDao;
+import com.bharath.spring.springmvcorm.user.entity.User;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,5 +18,11 @@ public class UserServiceImpl implements UserService {
 
 	public void setDao(UserDao dao) {
 		this.dao = dao;
+	}
+
+	@Override
+	public int save(User user) {
+		//Business Logic
+		return dao.create(user);
 	}
 }
