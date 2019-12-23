6709f872e1b034c8ddcbbdae41a87e81bcb7d68c
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 10:43:35 -0500

    Before advice implementation

--- springaop/src/main/java/com/bharath/spring/springaop/aspects/LogginAspect.java
+++ springaop/src/main/java/com/bharath/spring/springaop/aspects/LogginAspect.java
@@ -1,8 +1,14 @@
 package com.bharath.spring.springaop.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LogginAspect {
 
+	@Before("")
+	void logBefore(JoinPoint joinPoint) {
+		System.out.println("Before calling the method");
+	}
 }
