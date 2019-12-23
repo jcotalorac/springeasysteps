86c4a443426b8afc7159bedac2ede938e0ef0cdc
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 11:54:18 -0500

    Testing to get the service bean with autowired dependency

--- springjavaconfig/src/main/java/com/bharath/spring/springjavaconfig/Test.java
+++ springjavaconfig/src/main/java/com/bharath/spring/springjavaconfig/Test.java
@@ -7,8 +7,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-		Dao dao = context.getBean(Dao.class);
-		dao.create();
+		Service service = context.getBean(Service.class);
+		service.save();
 	}
 
 }
