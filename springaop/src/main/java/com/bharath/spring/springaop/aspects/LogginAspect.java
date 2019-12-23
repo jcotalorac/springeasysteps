211699a1521cee99d3b98cab17a6c2147f67db1b
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 10:46:58 -0500

    After advice implementation

--- springaop/src/main/java/com/bharath/spring/springaop/aspects/LogginAspect.java
+++ springaop/src/main/java/com/bharath/spring/springaop/aspects/LogginAspect.java
@@ -1,6 +1,7 @@
 package com.bharath.spring.springaop.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -10,5 +11,10 @@ public class LogginAspect {
 	@Before("")
 	void logBefore(JoinPoint joinPoint) {
 		System.out.println("Before calling the method");
+	}
+	
+	@After("")
+	void logAfter(JoinPoint joinPoint) {
+		System.out.println("After the method execution");
 	}
 }
