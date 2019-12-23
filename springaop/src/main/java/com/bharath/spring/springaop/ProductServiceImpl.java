f47180a6121345ce798eab2fac1de44c9fa4e1b5
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 10:35:02 -0500

    POJO and POJI for product service

---
+++ springaop/src/main/java/com/bharath/spring/springaop/ProductServiceImpl.java
@@ -0,0 +1,11 @@
+package com.bharath.spring.springaop;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public int multiply(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
