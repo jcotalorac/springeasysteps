2f9421892963f249d5bb95b78ea2de56cc7aa21a
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 16:20:03 -0500

    Component scan annotation

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
@@ -1,10 +1,12 @@
 package com.bharath.spring.springmvc.controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
+@ComponentScan(basePackages = "com.bharath.spring.springmvc.controller")
 @Configuration
 public class SpringConfig extends WebMvcConfigurerAdapter {
 
