d4301256c07baaca35efcfef9c8657c01a109e17
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 16:29:54 -0500

    Configuration method to get view resolver bean

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
@@ -1,13 +1,23 @@
 package com.bharath.spring.springmvc.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan(basePackages = "com.bharath.spring.springmvc.controller")
 @Configuration
 public class SpringConfig extends WebMvcConfigurerAdapter {
 
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
+	}
 }
