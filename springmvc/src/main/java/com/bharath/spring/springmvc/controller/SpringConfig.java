ed13cf9740174d51d9e12486d57dab36390181fd
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 16:16:28 -0500

    Annotation to enable mvc configuration

--- springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
@@ -1,7 +1,11 @@
 package com.bharath.spring.springmvc.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebMvc
+@Configuration
 public class SpringConfig extends WebMvcConfigurerAdapter {
 
 }
