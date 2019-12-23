e939c6c21eea559ce9558473fe25236e4390d716
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 12:01:35 -0500

    Importing spring configuration

--- springjavaconfig/src/main/java/com/bharath/spring/springjavaconfig/SpringConfig.java
+++ springjavaconfig/src/main/java/com/bharath/spring/springjavaconfig/SpringConfig.java
@@ -2,8 +2,10 @@ package com.bharath.spring.springjavaconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(DaoConfig.class)
 public class SpringConfig {
 
 	@Bean
