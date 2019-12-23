b11fd8eecfec06150e9ed9a836f88efde0f3cfb6
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 11:41:40 -0500

    Method to return dao beans in spring configuration class

--- springjavaconfig/src/main/java/com/bharath/spring/springjavaconfig/SpringConfig.java
+++ springjavaconfig/src/main/java/com/bharath/spring/springjavaconfig/SpringConfig.java
@@ -1,8 +1,13 @@
 package com.bharath.spring.springjavaconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
+	@Bean
+	public Dao dao() {
+		return new Dao();
+	}
 }
