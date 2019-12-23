63edda8fc922aa7b45cd5038ca3492d47842bd7d
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-23 16:15:17 -0500

    Class for spring beans configuration

---
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/SpringConfig.java
@@ -0,0 +1,7 @@
+package com.bharath.spring.springmvc.controller;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+}
