9f5e11aa6c91ed1b77a73ede38b6f61cf533fce8
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-24 11:34:50 -0500

    Web servlet configuration to replace web.xml

---
+++ springmvc/src/main/java/com/bharath/spring/springmvc/controller/WebServletConfiguration.java
@@ -0,0 +1,17 @@
+package com.bharath.spring.springmvc.controller;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+public class WebServletConfiguration implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+		webContext.register(SpringConfig.class);
+	}
+
+}
