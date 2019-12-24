7ea50ed37e5397e7601234c3867f66cba318c538
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-24 17:25:45 -0500

    Custom find by name

--- springdatajpa/src/main/java/com/bharath/spring/data/repository/ProductRepository.java
+++ springdatajpa/src/main/java/com/bharath/spring/data/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.bharath.spring.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.bharath.spring.data.entities.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
+	List<Product> findByName(String name);
 }
