997b59f5c7c12cdf46d45b4d002989982389e1bd
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-24 16:07:00 -0500

    Spring boot jpa starter project

---
+++ springdatajpa/src/test/java/com/bharath/spring/data/SpringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.spring.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
