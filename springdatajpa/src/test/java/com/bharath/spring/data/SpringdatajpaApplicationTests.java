ac4e311a5dd5136ec0dc87a3728b0c0e19b02ae8
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-24 16:56:41 -0500

    Test to save product

--- springdatajpa/src/test/java/com/bharath/spring/data/SpringdatajpaApplicationTests.java
+++ springdatajpa/src/test/java/com/bharath/spring/data/SpringdatajpaApplicationTests.java
@@ -1,13 +1,28 @@
 package com.bharath.spring.data;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.bharath.spring.data.entities.Product;
+import com.bharath.spring.data.repository.ProductRepository;
 
 @SpringBootTest
 class SpringdatajpaApplicationTests {
 
+	@Autowired
+	ApplicationContext context;
+	
 	@Test
-	void contextLoads() {
+	void saveProduct() {
+		ProductRepository productRepository = context.getBean(ProductRepository.class);
+		Product product = new Product();
+		product.setId(1L);
+		product.setName("Mac");
+		product.setDescription("awesome");
+		product.setPrice(1200d);
+		productRepository.save(product);
 	}
 
 }
