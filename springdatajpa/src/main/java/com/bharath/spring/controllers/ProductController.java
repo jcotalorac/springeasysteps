3930ca7e37537229bcf65d96b81c882463022c91
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-25 11:19:08 -0500

    Post method of rest controller to create product

--- springdatajpa/src/main/java/com/bharath/spring/controllers/ProductController.java
+++ springdatajpa/src/main/java/com/bharath/spring/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.bharath.spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +16,14 @@ public class ProductController {
 
 	@Autowired
 	ProductRepository productRepository;
-	
+
 	@GetMapping
 	public Iterable<Product> getProducts() {
 		return productRepository.findAll();
+	}
+
+	@PostMapping
+	public Product create(@RequestBody Product product) {
+		return productRepository.save(product);
 	}
 }
