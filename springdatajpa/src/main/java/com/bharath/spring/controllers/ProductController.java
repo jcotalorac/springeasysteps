018c728a8eb8eb4227c558438e84253f36010c63
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-25 11:22:33 -0500

    Update method of rest controller

--- springdatajpa/src/main/java/com/bharath/spring/controllers/ProductController.java
+++ springdatajpa/src/main/java/com/bharath/spring/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.bharath.spring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,11 @@ public class ProductController {
 
 	@PostMapping
 	public Product create(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
+	
+	@PutMapping
+	public Product update(@RequestBody Product product) {
 		return productRepository.save(product);
 	}
 }
