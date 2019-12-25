fed47e448997605204a7d795c1db50f075d0a871
Juan Otalora <juancarlos.otalora@wnco.com>
2019-12-25 11:04:36 -0500

    Get method to retrieve all products

--- springdatajpa/src/main/java/com/bharath/spring/controllers/ProductController.java
+++ springdatajpa/src/main/java/com/bharath/spring/controllers/ProductController.java
@@ -1,10 +1,22 @@
 package com.bharath.spring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bharath.spring.data.entities.Product;
+import com.bharath.spring.data.repository.ProductRepository;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+	@Autowired
+	ProductRepository productRepository;
+	
+	@GetMapping
+	public Iterable<Product> getProducts() {
+		return productRepository.findAll();
+	}
 }
